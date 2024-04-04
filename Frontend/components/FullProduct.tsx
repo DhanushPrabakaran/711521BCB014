23bd25f464f1e7d72276f52f3758c002e429ea58
Dhanush Prabakaran <dhanushprabakaran18@gmail.com>
2024-04-04 13:32:11 +0530

    update

--- Frontend/components/FullProduct.tsx
+++ Frontend/components/FullProduct.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
-
 interface Product {
     _id: number;
     ProductName: string;
     price: Number;
     rating: number;
-    discount:Number;
+    discount: Number;
     availablity: Boolean;
-    
-  }
-const FullProduct = () => {
-  return (
-    <div>
+}
 
-    </div>
-  )
+const FullProduct: React.FC<{ product: Product }> = ({ product }) => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h2 className="card-title">{product.ProductName}</h2>
+                <p>Price: {product.price.toString()}</p>
+                <p>Rating: {product.rating}</p>
+                <p>Discount: {product.discount.toString()}</p>
+                <p>Availability: {product.availablity ? "Available" : "Not available"}</p>
+            </div>
+        </div>
+    )
 }
 
 export default FullProduct
